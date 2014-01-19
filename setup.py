512f3969b4de798af6e484e9a666fa8e2fd7e233
Michael Bauer <mihi@lo-res.org>
2014-01-19 20:25:33 +0100

    added setup file for installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='mnmlwrt',
+    version='0.02',
+    description='A minimum distraction editor',
+    author='Michael Bauer',
+    url='http://mnmlwrt.tentacleriot.eu/',
+    scripts=['mnmlwrt'],
+    )
+
